d68371b6ae686ba29141dd59f1fd6f37615bf673
DieMyst <dmitry.shakhtarin@fluence.ai>
2019-07-02 12:34:27 +0300

    doc

--- hashsign/js/src/main/scala/fluence/crypto/ecdsa/Ecdsa.scala
+++ hashsign/js/src/main/scala/fluence/crypto/ecdsa/Ecdsa.scala
@@ -37,18 +37,20 @@ import scala.scalajs.js.typedarray.Uint8Array
 class Ecdsa(ec: EC, hasher: Option[Crypto.Hasher[Array[Byte], Array[Byte]]]) {
   import CryptoError.nonFatalHandling
 
+  /**
+   * Restores key pair by secret key.
+   *
+   */
   def restoreKeyPair[F[_]](secretKey: KeyPair.Secret)(implicit F: Monad[F]): EitherT[F, CryptoError, KeyPair] = {
     for {
-      secret <- {
-        nonFatalHandling {
-          val key = ec.keyFromPrivate(secretKey.value.toHex, "hex")
-          val publicHex = key.getPublic(compact = true, "hex")
-          val secretHex = key.getPrivate("hex")
-          val public = ByteVector.fromValidHex(publicHex)
-          val secret = ByteVector.fromValidHex(secretHex)
-          KeyPair.fromByteVectors(public, secret)
-        }("Incorrect secret key format")
-      }
+      secret ← nonFatalHandling {
+        val key = ec.keyFromPrivate(secretKey.value.toHex, "hex")
+        val publicHex = key.getPublic(compact = true, "hex")
+        val secretHex = key.getPrivate("hex")
+        val public = ByteVector.fromValidHex(publicHex)
+        val secret = ByteVector.fromValidHex(secretHex)
+        KeyPair.fromByteVectors(public, secret)
+      }("Incorrect secret key format")
     } yield secret
   }
 
